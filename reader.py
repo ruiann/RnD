38c080e53a56feef2754297da96a0d2478780d51
inverse <534676033@qq.com>
2017-05-23 15:51:47 +0800

    use json to solve unalignment problem

--- reader.py
+++ reader.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import numpy as np
 import os
+import json
 
 data_dir = './OLHWDB1.1trn_pot_decode'
 target_dir = './data'
@@ -12,31 +13,38 @@ bucket_num = 10
 
 
 def pad(length, data):
-    pad = length - len(data)
-    if pad > 0:
-        eos = np.array([[0, 0, 0, 0, 1]] * pad, np.float32)
+    pad_length = length - len(data)
+    if pad_length > 0:
+        eos = np.array([[0, 0, 0, 0, 1]] * pad_length, np.float32)
         data = np.concatenate((data, eos), axis=0)
     else:
         data = data[0: length]
-    return data
+    return np.array(data, np.float32)
 
 
 def norm(sequence, std=None):
     sequence = np.array(sequence, dtype=np.float32)
     mean = sequence.mean()
     std = std or sequence.std()
-    sequence = 100 * (sequence - mean) / std
+    sequence = (sequence - mean) / std
     return sequence, std
 
 
 def read_file(path):
+    new_data = []
     data = np.loadtxt(path)
     data[:, 0], std = norm(data[:, 0])
     data[:, 1], _ = norm(data[:, 1], std)
+    prev_x = 0
+    prev_y = 0
+    for point in data:
+        new_data.append([point[0] - prev_x, point[1] - prev_y, point[2], point[3], point[4]])
+        prev_x = point[0]
+        prev_y = point[1]
     bucket_index = min(int(len(data) / bucket_gap), bucket_num - 1)
     length = bucket_gap * (bucket_index + 1)
-    data = pad(length, data)
-    return data, bucket_index
+    data = pad(length, new_data)
+    return data.tolist(), bucket_index
 
 
 def init_bucket():
@@ -47,24 +55,36 @@ def init_bucket():
 
 
 def init():
-    buckets = init_bucket()
+    data_buckets = init_bucket()
+    label_buckets = init_bucket()
     index = 0
     for label in os.listdir(data_dir):
-        print(label)
         label_dir = os.path.join(data_dir, label)
         for writer in os.listdir(label_dir):
             data, bucket_index = read_file(os.path.join(label_dir, writer))
-            data = {'data': data, 'label': index}
-            buckets[bucket_index].append(data)
+            label_buckets[bucket_index].append(index)
+            data_buckets[bucket_index].append(data)
         index = index + 1
     for i in range(bucket_num):
-        np.savetxt(os.path.join(target_dir, '{}.txt'.format(i)), buckets[i])
-    return buckets, index
+        with open('{}/data_bucket_{}.json'.format(target_dir, i), 'w') as json_file:
+            json_file.write(json.dumps(data_buckets[i]))
+        with open('{}/label_bucket_{}.json'.format(target_dir, i), 'w') as json_file:
+            json_file.write(json.dumps(label_buckets[i]))
+    return data_buckets, label_buckets, index
+
+
+def read():
+    data_buckets = []
+    label_buckets = []
+    for i in range(bucket_num):
+        with open('{}/data_bucket_{}.json'.format(target_dir, i)) as json_file:
+            data = json.load(json_file)
+            data_buckets.append(data)
+        with open('{}/label_bucket_{}.json'.format(target_dir, i)) as json_file:
+            label = json.load(json_file)
+            label_buckets.append(label)
+    return data_buckets, label_buckets
 
 
 if __name__ == '__main__':
-    data, class_num = init()
-    print(class_num)
-    print(len(data))
-    print(len(data[0]))
-    print(len(data[0][0]))
+    read()
