bba046a544bae45e668744922d8aeaa4b2815a9b
inverse <534676033@qq.com>
2017-05-31 11:55:34 +0800

    update model

--- BidirectionalLSTM.py
+++ BidirectionalLSTM.py
@@ -3,28 +3,34 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.contrib import rnn
 
 
 class BidirectionalLSTM:
-    def __init__(self, name, lstm_size, data_type=tf.float32):
+    def __init__(self, name, lstm_size, data_type=tf.float32, stack=1):
         self.data_type = data_type
         self.name = name
         with tf.variable_scope(self.name):
-            self.forward_lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-            self.backward_lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+            self.forward_lstm = rnn.MultiRNNCell([rnn.BasicLSTMCell(lstm_size[i]) for i in range(stack)])
+            self.backward_lstm = rnn.MultiRNNCell([rnn.BasicLSTMCell(lstm_size[i]) for i in range(stack)])
 
-    def run(self, data, reuse=False, time_major=False):
+    def run(self, data, reuse=False, time_major=False, pooling=False):
+        time_axis = 0 if time_major else 1
         with tf.variable_scope(self.name):
             with tf.variable_scope("ForwardLSTM", reuse=reuse) as scope:
-                time_axis = 0 if time_major else 1
                 forward_output, state = tf.nn.dynamic_rnn(self.forward_lstm, data, dtype=self.data_type, time_major=time_major, scope=scope)
-                forward_output = tf.reduce_mean(forward_output, time_axis)
+                if pooling == 'mean':
+                    forward_output = tf.reduce_mean(forward_output, time_axis)
+                else:
+                    forward_output = forward_output[-1, :, :] if time_major else forward_output[:, -1, :]
 
             with tf.variable_scope("BackwardLSTM", reuse=reuse) as scope:
-                time_axis = 0 if time_major else 1
                 data = tf.reverse(data, axis=[time_axis])
                 backward_output, state = tf.nn.dynamic_rnn(self.backward_lstm, data, dtype=self.data_type, time_major=time_major, scope=scope)
-                backward_output = tf.reduce_mean(backward_output, time_axis)
+                if pooling == 'mean':
+                    backward_output = tf.reduce_mean(backward_output, time_axis)
+                else:
+                    backward_output = backward_output[-1, :, :] if time_major else backward_output[:, -1, :]
 
             tf.summary.histogram('forward_lstm_output', forward_output)
             tf.summary.histogram('backward_lstm_output', backward_output)
--- encoder.py
+++ encoder.py
@@ -9,9 +9,9 @@ from BidirectionalLSTM import BidirectionalLSTM
 
 class Encoder:
 
-    def __init__(self, lstm_size=800, class_num=10):
-        self.bidirectional_LSTM = BidirectionalLSTM('BidirectionalLSTM', lstm_size)
-        self.logistic_regression = LogisticRegression('LogisticRegression', lstm_size, class_num)
+    def __init__(self, lstm=[100, 500], class_num=10):
+        self.bidirectional_LSTM = BidirectionalLSTM('BidirectionalLSTM', lstm, stack=2)
+        self.logistic_regression = LogisticRegression('LogisticRegression', lstm[-1], [800, class_num])
 
     # do classification
     def run(self, data):
@@ -19,21 +19,21 @@ class Encoder:
         return self.regression(lstm_code)
 
     def lstm(self, data):
-        return self.bidirectional_LSTM.run(data, False)
+        return self.bidirectional_LSTM.run(data, reuse=False, time_major=False)
 
     def regression(self, lstm_code):
-        return tf.nn.relu(self.logistic_regression.run(lstm_code))
+        return tf.nn.relu(self.logistic_regression.run(lstm_code)[-1])
 
     # compute loss
     def loss(self, logits, labels):
         return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='RHS'))
 
     # return training operation, data should be a PlaceHolder
-    def train(self, rate, data, labels):
+    def train(self, data, labels):
         logits = self.run(data)
         classification = tf.to_int32(tf.arg_max(tf.nn.softmax(logits), dimension=1))
         differ = labels - classification
         tf.summary.histogram('classification difference', differ)
         loss = self.loss(logits, labels)
         tf.summary.scalar('classifier loss', loss)
-        return tf.train.AdamOptimizer(learning_rate=rate).minimize(loss)
+        return loss
