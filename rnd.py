470852a7aafe3b60bbf6677fbf4d3da9168ccb18
inverse <534676033@qq.com>
2017-06-29 13:55:04 +0800

    bug fix, able to train recognizer

--- rnd.py
+++ rnd.py
@@ -7,7 +7,7 @@ from tensorflow.contrib import rnn
 
 
 class RnD:
-    def __init__(self, batch_size=32, data_type=tf.float32, label=350):
+    def __init__(self, batch_size=32, data_type=tf.float32, label=3755):
         self.batch_size = batch_size
         self.data_type = data_type
         self.label = label
@@ -18,27 +18,24 @@ class RnD:
         with tf.variable_scope('encoder', reuse=reuse):
             forward_cell = rnn.MultiRNNCell([rnn.GRUCell(rnn_size[i]) for i in range(len(rnn_size))])
             backward_cell = rnn.MultiRNNCell([rnn.GRUCell(rnn_size[i]) for i in range(len(rnn_size))])
-            forward_output, _ = tf.nn.dynamic_rnn(forward_cell, x, dtype=self.data_type, time_major=time_major)
-            backward_output, _ = tf.nn.dynamic_rnn(backward_cell, x, dtype=self.data_type, time_major=time_major)
+            output, state = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, x, dtype=self.data_type, time_major=time_major)
+            forward_output, backward_output = output
 
             if pooling == 'mean':
                 forward_output = tf.reduce_mean(forward_output, time_axis)
-            else:
-                forward_output = forward_output[-1, :, :] if time_major else forward_output[:, -1, :]
-
-            if pooling == 'mean':
                 backward_output = tf.reduce_mean(backward_output, time_axis)
             else:
+                forward_output = forward_output[-1, :, :] if time_major else forward_output[:, -1, :]
                 backward_output = backward_output[-1, :, :] if time_major else backward_output[:, -1, :]
 
             tf.summary.histogram('forward_rnn_output', forward_output)
             tf.summary.histogram('backward_rnn_output', backward_output)
 
             code = (forward_output + backward_output) / 2
-            with tf.variable_scope('encoder_regression'):
+            with tf.variable_scope('encoder_classification'):
                 logit = full_connection_layer(code, self.label)
 
             tf.summary.histogram('classification', logit)
 
-        self.encode_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
+        self.encoder_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
         return logit
--- train.py
+++ train.py
@@ -17,23 +17,23 @@ data_buckets, label_buckets = read()
 
 
 def feed_dict(batch_size):
-    s_feed = []
+    x_feed = []
     label_feed = []
     bucket_index = random.randint(0, len(data_buckets) - 1)
     data_bucket = data_buckets[bucket_index]
-    label_bucket = data_buckets[bucket_index]
+    label_bucket = label_buckets[bucket_index]
     for i in range(batch_size):
         index = random.randint(0, len(data_bucket) - 1)
         data = data_bucket[index]
         label = label_bucket[index]
-        s_feed.append(data)
+        x_feed.append(data)
         label_feed.append(label)
-    return bucket_index, s_feed, label_feed
+    return bucket_index, x_feed, label_feed
 
 
 def train():
     x = tf.placeholder(tf.float32, [batch_size, None, 5])
-    label = tf.placeholder(tf.float32, [batch_size])
+    label = tf.placeholder(tf.int32, [batch_size])
     model = RnD(batch_size=batch_size)
 
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
@@ -42,7 +42,7 @@ def train():
         global_step = tf.Variable(0, name='global_step')
         update_global_step = tf.assign(global_step, global_step + 1)
 
-        logits = model.rnn_encode(x, batch_size)
+        logits = model.rnn_encode(x)
         classification = tf.to_int32(tf.arg_max(tf.nn.softmax(logits), dimension=1))
         differ = label - classification
         tf.summary.histogram('classification difference', differ)
@@ -68,11 +68,9 @@ def train():
             summary_str, loss = sess.run([summary, train_op], feed_dict={x: x_batch, label: label_batch})
             summary_writer.add_summary(summary_str, step)
 
-            if global_step.eval() % 20 == 0:
+            if step % 10000 == 0 and step != 0:
                 checkpoint_file = os.path.join(model_dir, 'model.latest')
                 saver.save(sess, checkpoint_file)
-
-            if global_step.eval() % 100 == 0:
                 summary_writer.add_run_metadata(run_metadata, 'step%03d' % step)
 
             sess.run(update_global_step)
